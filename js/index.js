1cfae250383795e2202c3176929481acc8d063f3
Brian767 <brra7685@colorado.edu>
2025-08-27 13:06:07 -0600

    make logo click

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-window.addEventListener('scroll', function() {
-    const contactBar = document.querySelector('.contact-bar');
-    if (!contactBar) return;
+window.addEventListener("scroll", function () {
+  const contactBar = document.querySelector(".contact-bar");
+  if (!contactBar) return;
 
-    if (window.scrollY > 0) {
-        contactBar.style.boxShadow = '0 2px 10px rgba(0, 0, 0, .2)';
-    } else {
-        contactBar.style.boxShadow = 'none';
-    }
+  if (window.scrollY > 5) {
+    contactBar.style.boxShadow = "0 2px 10px rgba(0, 0, 0, .2)";
+    contactBar.style.background = "rgba(255, 255, 255, 1)";
+  } else {
+    contactBar.style.boxShadow = "none";
+    contactBar.style.background = "none";
+  }
 });
